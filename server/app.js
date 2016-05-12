807e3af292f91aa8cffe03b675010e8bbc9b8dff
Daniel Chavez <d.chavez562@yahoo.com>
2016-05-12 16:49:04 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,34 @@
+// Node Server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var felines = require('./routes/felines');
+
+app.set('port', 5000);
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Data
+
+
+//ROUTES
+app.use('/felines', felines);
+// app.get('/felines', function(req, res) {
+//   res.send(cats);
+// });
+//
+// app.post('/felines', function(req, res) {
+//   cats.push(req.body.catName);
+//   res.send(true);
+// });
+
+app.use('/', index);
+
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server is ready on port:' + app.get('port'));
+});
